e74eaf3097d5818b45718eb1114a6cb753c6efe1
ZhuoerFeng <RealAndrewFeng@163.com>
2023-05-23 22:52:23 +0800

    fix data_util problems

--- applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/test.py
+++ applications/DeepSpeed-Chat/training/step3_rlhf_finetuning/test.py
@@ -1,6 +1,7 @@
 from datasets import load_dataset
 
-dataset = load_dataset('/data/fze/dataset/PKU-SafeRLHF-10K')
+dataset = load_dataset('/data/fze/dataset/PKU-SafeRLHF-10K')['train'].train_test_split(test_size=0.1)
 print(dataset.column_names)
-train_set = dataset['train']
-print(train_set[0])
+# train_set = dataset['train'].train_test_split(test_size=0.1)
+print(dataset['train'])
+print(len(dataset['train']))
